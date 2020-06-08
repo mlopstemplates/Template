cc5c5ad3355f9f824fc6356f4b16e7c543fa84b9
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-08 17:05:25 +0530

    Update configuration.sh

--- configuration.sh
+++ configuration.sh
@@ -1,3 +1,2 @@
-echo '::set-env name=TEMPLATE_FILE::azuredeploy.json'
-echo '::set-env name=RESOURCE_GROUP::ExampleGroup'
-
+echo '::set-env name=PAT_TOKEN::123456789'
+echo '::set-env name=RESOURCE_GROUP::TestGroup'
