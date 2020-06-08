9d088b3c2d7b21acd4aa9cd9a187fd955850d62d
vivishno <64971172+vivishno@users.noreply.github.com>
2020-06-08 17:17:03 +0530

    Update sampleScript.sh

--- sampleScript.sh
+++ sampleScript.sh
@@ -1,2 +1,2 @@
 az deployment group create --resource-group $RESOURCE_GROUP --template-file azuredeploy.json --parameters azuredeploy.params.json
-az functionapp config appsettings set --name fappdeploykyspm --resource-group $RESOURCE_GROUP --settings "PATTOKEN=$PAT_TOKEN"
+az functionapp config appsettings set --name fappdeployejmet --resource-group $RESOURCE_GROUP --settings "PATTOKEN=$PAT_TOKEN"
